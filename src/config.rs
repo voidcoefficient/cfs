6598c17b0849e0a4b5e4106a74b16e8a0f9f61f7
lzldev <francisco.lucas0507@gmail.com>
2026-03-31 08:03:10 -0300

    fix: rustfmt file and run cargo fmt --all

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 use std::{env::home_dir, path::PathBuf};
 
 pub fn get_config_path() -> PathBuf {
-    let home_folder = home_dir().expect("couldn't find home directory");
-    let path = home_folder.join(".cfs.json");
+	let home_folder = home_dir().expect("couldn't find home directory");
+	let path = home_folder.join(".cfs.json");
 
-    return path;
+	return path;
 }
