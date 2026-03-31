fa7033cafd400156436873546449d887918be4b2
lzldev <38144471+lzldev@users.noreply.github.com>
2026-03-31 08:43:01 -0300

    fix: use package name on error message

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,10 @@
 use std::process::exit;
 
 pub fn invalid(cause: &str) {
-	eprintln!("invalid {}. get help by running `conf set --help`", cause);
+	eprintln!(
+		"invalid {}. get help by running `{} --help`",
+		cause,
+		env!("CARGO_PKG_NAME")
+	);
 	exit(1);
 }
