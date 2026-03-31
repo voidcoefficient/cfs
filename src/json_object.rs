188c62aa19347e56562a718c965c18b20c3626a5
lzldev <38144471+lzldev@users.noreply.github.com>
2026-03-31 08:03:10 -0300

    fix: rustfmt file and run cargo fmt --all

--- src/json_object.rs
+++ src/json_object.rs
@@ -8,38 +8,38 @@ use json::JsonValue;
 use crate::config::get_config_path;
 
 pub fn get_json_object_or_create(force_create: bool) -> JsonValue {
-    let path_exists = &get_config_path().exists();
+	let path_exists = &get_config_path().exists();
 
-    if force_create && !path_exists {
-        let mut file = File::create(get_config_path()).unwrap();
-        write!(file, "{}", "{}").unwrap();
-    }
+	if force_create && !path_exists {
+		let mut file = File::create(get_config_path()).unwrap();
+		write!(file, "{}", "{}").unwrap();
+	}
 
-    get_json_object()
+	get_json_object()
 }
 
 pub fn get_json_object() -> JsonValue {
-    let path = get_config_path();
+	let path = get_config_path();
 
-    if !path.exists() {
-        eprintln!("config file does not exist at '{:?}'", &path);
-        exit(1);
-    }
+	if !path.exists() {
+		eprintln!("config file does not exist at '{:?}'", &path);
+		exit(1);
+	}
 
-    let json = json::parse(&*read_to_string(&path).unwrap()).unwrap();
+	let json = json::parse(&*read_to_string(&path).unwrap()).unwrap();
 
-    if !json.is_object() {
-        eprintln!("config file is not a JSON file ('{:?}')", &path);
-        exit(1);
-    }
+	if !json.is_object() {
+		eprintln!("config file is not a JSON file ('{:?}')", &path);
+		exit(1);
+	}
 
-    return json;
+	return json;
 }
 
 pub fn set_json_object(json: JsonValue) -> io::Result<()> {
-    let mut file = File::create(get_config_path())?;
-    let json_string = json::stringify_pretty(json, 2);
-    write!(file, "{}", json_string)?;
+	let mut file = File::create(get_config_path())?;
+	let json_string = json::stringify_pretty(json, 2);
+	write!(file, "{}", json_string)?;
 
-    Ok(())
+	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ mod flags;
 mod json_object;
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let app = App::new(env!("CARGO_PKG_NAME"))
-        .description(env!("CARGO_PKG_DESCRIPTION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .usage(format!("{} [commands]", env!("CARGO_PKG_NAME")))
-        .command(set_value())
-        .command(get_value())
-        .command(list())
-        .command(init())
-        .command(remove_value())
-        .command(clear());
+	let args: Vec<String> = env::args().collect();
+	let app = App::new(env!("CARGO_PKG_NAME"))
+		.description(env!("CARGO_PKG_DESCRIPTION"))
+		.author(env!("CARGO_PKG_AUTHORS"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.usage(format!("{} [commands]", env!("CARGO_PKG_NAME")))
+		.command(set_value())
+		.command(get_value())
+		.command(list())
+		.command(init())
+		.command(remove_value())
+		.command(clear());
 
-    app.run(args);
+	app.run(args);
 
-    Ok(())
+	Ok(())
 }
